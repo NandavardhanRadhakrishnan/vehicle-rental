f98347a339b97d984446253a205c98ebcc19e577
NandavardhanRadhakrishnan <nr3648@srmist.edu.in>
2022-06-18 14:16:15 +0530

    added bill printing

--- vehicle_rental.c
+++ vehicle_rental.c
@@ -7,3 +7,27 @@ struct vehicle
     int price_per_hour;
     int min_hours;
 };
+
+int calc_amt(struct vehicle v, int hours)
+{
+    int amt;
+    if (hours < v.min_hours)
+    {
+        amt = v.price_per_hour * v.min_hours;
+    }
+    else
+    {
+        amt = v.price_per_hour * hours;
+    }
+    return amt;
+}
+
+int print_bill(struct vehicle v, int hours)
+{
+    printf("\n\n\n");
+    printf("vehicle selected:%s\n", v.name);
+    printf("hours of rental:%d\n", hours);
+    printf("amount due:$%d\n", calc_amt(v, hours));
+    printf("\n\n\n");
+    return 0;
+}
